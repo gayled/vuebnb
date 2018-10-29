852fa3411acfc34239b28788a65720bacf3d5176
Gayle DiCarlantonio <gayledmail@gmail.com>
2018-10-28 19:44:01 -0700

    added server-side route to home page

--- app/Http/Controllers/ListingController.php
+++ app/Http/Controllers/ListingController.php
@@ -28,4 +28,10 @@ class ListingController extends Controller
 		$model = $this->add_image_urls($model, $listing->id);
 		return view('app', ['model' => $model]);
 	}
+
+	public function get_home_web() 
+	{
+		return view('app', ['model' => []
+		]);
+	}
 }
