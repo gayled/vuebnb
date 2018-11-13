fa3aca324dee16c1b4ee65621f60c63a8565ab71
Gayle DiCarlantonio <gayledmail@gmail.com>
2018-11-12 19:07:38 -0800

    added user login

--- routes/api.php
+++ routes/api.php
@@ -20,3 +20,4 @@
 Route::get('/listing/{listing}', 'ListingController@get_listing_api');
 Route::get('/', 'ListingController@get_home_api');
 Route::get('/saved', 'ListingController@get_home_api');
+Route::post('/user/toggle_saved', 'UserController@toggle_saved')->middleware('auth:api');
